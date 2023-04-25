8204c25297a244ddfca7ea24046eca80008222f8
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-24 21:18:14 -0300

    feat: Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Meu Portfolio | 2023</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
     
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>
-        Portfolio 2023
+        Home
       </h1>
     </div>
   )
